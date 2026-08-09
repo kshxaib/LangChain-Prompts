20a190ff06ec4a2bd0e1eedf5724a018a9b39f91
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-08-09 19:43:31 +0530

    learning message place holder

---
+++ message_placeholder.py
@@ -0,0 +1,24 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from dotenv import load_dotenv
+from langchain_core.messages import AIMessage
+
+
+chat_template = ChatPromptTemplate([
+    ('system', "You are a helpful customer support agent."),
+    MessagesPlaceholder(variable_name='chat_history'),
+    ('human', '{query}')
+])
+
+chat_history = []
+
+with open('chat_history.txt') as f:
+    chat_history.extend(f.readlines())
+        
+
+
+print(chat_history)
+
+prompt = chat_template.invoke({'chat_history': chat_history, 'query': 'Where is my refund for my order?'})
+
+
+print(prompt)

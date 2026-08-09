47321edb7a7d349444540d89144d606a42349789
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-08-09 13:49:28 +0530

    chain

--- prompt_ui.py
+++ prompt_ui.py
@@ -1,7 +1,7 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, load_prompt
 
 load_dotenv()
 model = ChatOpenAI()
@@ -17,40 +17,14 @@ style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "T
 # User selects the explanation length
 length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
 
+template = load_prompt('template.json')
 
-# PromptTemplate = reusable template for creating dynamic prompts
-# {paper_input}, {style_input}, {length_input} are dynamic variables
-template = PromptTemplate(
-   template="""
-      Please summarize the research paper titled "{paper_input}" with the following specifications:
-         Explanation Style: {style_input}  
-         Explanation Length: {length_input}  
-      1. Mathematical Details:  
-            - Include relevant mathematical equations if present in the paper.  
-            - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  
-      2. Analogies:  
-            - Use relatable analogies to simplify complex ideas.  
-      If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.  
-      Ensure the summary is clear, accurate, and aligned with the provided style and length.
-    """,
-
-   # Variables that will be replaced with actual user input
-   input_variables=['paper_input', 'style_input','length_input'],
-
-   # Validates that template variables match input_variables
-   validate_template=True
-)
-
-
-# invoke() fills the PromptTemplate variables with actual values
-# Creates the final dynamic prompt
-prompt = template.invoke({
+if st.button("Explain"):
+   chain = template | model
+   result = chain.invoke({
    'paper_input': paper_input,
    'style_input':style_input,
    'length_input':length_input
-})
+   })
 
-
-if st.button("Explain"):
-   response = model.invoke(prompt)  # Send the generated prompt to the Chat Model
-   st.write(response.content)
+   st.write(result.content)

b20e0747bfaae182829117a0c77abf692ba1b671
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-08-09 18:37:06 +0530

    simple chat bot

---
+++ chat.py
@@ -0,0 +1,20 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+chat_history = []
+
+while True:
+    user_input = input('User Input: ')
+    chat_history.append(user_input)
+
+    if user_input == 'exit':
+        break
+
+    response = model.invoke(chat_history)
+    chat_history.append(response.content)
+
+    print("AI: " + response.content)

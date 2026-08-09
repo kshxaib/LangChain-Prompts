b92de4b25f3923b0b158da42ddaa6b2e1123e6c7
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-08-09 18:55:54 +0530

    add messages in chatbot

--- chat.py
+++ chat.py
@@ -1,20 +1,23 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 
 load_dotenv()
 
 model = ChatOpenAI()
 
-chat_history = []
+chat_history = [
+    SystemMessage(content="You are a helpful assistant")
+]
 
 while True:
     user_input = input('User Input: ')
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content=user_input))
 
     if user_input == 'exit':
         break
 
     response = model.invoke(chat_history)
-    chat_history.append(response.content)
+    chat_history.append(AIMessage(content=response.content))
 
     print("AI: " + response.content)
